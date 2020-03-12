cb98a69a0681569a1e1f643bacc223277df2fa55
scOwez <owez@scalist.net>
2020-03-12 15:46:11 +0000

    WIP newslets api

--- collecter.py
+++ collecter.py
@@ -9,10 +9,12 @@ from collections import defaultdict
 from pprint import pprint as pp
 from pathlib import Path
 
+
 class DateTimeEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, (datetime.date, datetime.datetime)):
             return obj.isoformat()
+
 
 def delglobal():
     mypath = "./global_daily"
@@ -20,159 +22,227 @@ def delglobal():
         for file in files:
             os.remove(os.path.join(root, file))
 
+
 def get_most_recent():
-    url = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
+    url = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports"
     page = requests.get(url)
-    soup=BeautifulSoup(page.content,'html.parser')
-    table = soup.find('table', attrs={'class': 'files js-navigation-container js-active-navigation-container'})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    data=[]
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find(
+        "table",
+        attrs={"class": "files js-navigation-container js-active-navigation-container"},
+    )
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
+    data = []
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
         data.append([ele for ele in cols if ele])
     lastelement = data[-2][0]
-    lastelementurl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}'.format(lastelement)
+    lastelementurl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}".format(
+        lastelement
+    )
     recent = requests.get(lastelementurl)
-    #print(recent.text)
+    # print(recent.text)
     return recent.text
 
 
 def returncsv():
-    with open('data.csv', 'w') as file:
+    with open("data.csv", "w") as file:
         file.write(get_most_recent())
-    pd.set_option('display.max_rows', None)
-    df = pd.read_csv('./data.csv',usecols=['Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered', 'Latitude','Longitude'])
+    pd.set_option("display.max_rows", None)
+    df = pd.read_csv(
+        "./data.csv",
+        usecols=[
+            "Country/Region",
+            "Last Update",
+            "Confirmed",
+            "Deaths",
+            "Recovered",
+            "Latitude",
+            "Longitude",
+        ],
+    )
     return df
 
+
 def csvtojson():
-    fieldnames = ('Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered', 'Latitude','Longitude')
+    fieldnames = (
+        "Country/Region",
+        "Last Update",
+        "Confirmed",
+        "Deaths",
+        "Recovered",
+        "Latitude",
+        "Longitude",
+    )
     data = returncsv()
     countries = []
-    for i in data['Country/Region']:
+    for i in data["Country/Region"]:
         if i not in countries:
             countries.append(i)
     datadict = {}
     for country in countries:
-        confirmed = data[data['Country/Region'] ==  country]['Confirmed']
+        confirmed = data[data["Country/Region"] == country]["Confirmed"]
         totalconfirmed = 0
         for conf in confirmed:
             totalconfirmed += conf
-        deaths = data[data['Country/Region'] == country]['Deaths']
+        deaths = data[data["Country/Region"] == country]["Deaths"]
         totaldeaths = 0
         for death in deaths:
             totaldeaths += death
-        recoveries = data[data['Country/Region'] == country]['Recovered']
+        recoveries = data[data["Country/Region"] == country]["Recovered"]
         totalrecoveries = 0
         for recovery in recoveries:
             totalrecoveries += recovery
-        latitudelist = data[data['Country/Region'] == country]['Latitude']
+        latitudelist = data[data["Country/Region"] == country]["Latitude"]
         count = 0
         totallatitudes = 0
         for latitude in latitudelist:
             count += 1
             totallatitudes += latitude
-        avlatitude = float(totallatitudes/count)
-        longitudelist = data[data['Country/Region'] == country]['Longitude']
+        avlatitude = float(totallatitudes / count)
+        longitudelist = data[data["Country/Region"] == country]["Longitude"]
         count = 0
         totallongitudes = 0
         for longitude in longitudelist:
             count += 1
             totallongitudes += longitude
-        avtotallongitudes = float(totallongitudes/count)
+        avtotallongitudes = float(totallongitudes / count)
 
         datadict[country] = {
-                     'LastUpdate': datetime.datetime.now().utcnow(),
-                     'ConfirmedCases': totalconfirmed,
-                     'Deaths': totaldeaths,
-                     'Recovered': totalrecoveries
-                     }
-    with open('data.json', 'w') as file:
+            "LastUpdate": datetime.datetime.now().utcnow(),
+            "ConfirmedCases": totalconfirmed,
+            "Deaths": totaldeaths,
+            "Recovered": totalrecoveries,
+        }
+    with open("data.json", "w") as file:
         file.write(json.dumps(datadict, cls=DateTimeEncoder))
 
     data = json.dumps(datadict, cls=DateTimeEncoder)
     return data
 
+
 def csvtojsonfunction(data, name):
-    fieldnames = ('Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered', 'Latitude','Longitude')
+    fieldnames = (
+        "Country/Region",
+        "Last Update",
+        "Confirmed",
+        "Deaths",
+        "Recovered",
+        "Latitude",
+        "Longitude",
+    )
     countries = []
-    for i in data['Country/Region']:
+    for i in data["Country/Region"]:
         if i not in countries:
             countries.append(i)
     datadict = {}
-    name = name.split('.csv')[0]
+    name = name.split(".csv")[0]
     datadict[name] = {}
     for country in countries:
-        confirmed = data[data['Country/Region'] ==  country]['Confirmed']
+        confirmed = data[data["Country/Region"] == country]["Confirmed"]
         totalconfirmed = 0
         for conf in confirmed:
             totalconfirmed += conf
-        deaths = data[data['Country/Region'] == country]['Deaths']
+        deaths = data[data["Country/Region"] == country]["Deaths"]
         totaldeaths = 0
         for death in deaths:
             totaldeaths += death
-        recoveries = data[data['Country/Region'] == country]['Recovered']
+        recoveries = data[data["Country/Region"] == country]["Recovered"]
         totalrecoveries = 0
         for recovery in recoveries:
             totalrecoveries += recovery
-        datadict[name].update({ country:
-                        {
-                        'ConfirmedCases': int(totalconfirmed),
-                        'Deaths': int(totaldeaths),
-                        'Recovered': int(totalrecoveries)
-                        }
-                        })
-    with open('global_daily/{}.json'.format(name), 'w') as file:
+        datadict[name].update(
+            {
+                country: {
+                    "ConfirmedCases": int(totalconfirmed),
+                    "Deaths": int(totaldeaths),
+                    "Recovered": int(totalrecoveries),
+                }
+            }
+        )
+    with open("global_daily/{}.json".format(name), "w") as file:
         file.write(json.dumps(datadict, cls=DateTimeEncoder))
+
 
 def get_data_from_all_to_json():
     delglobal()
-    if os.path.exists('totaldata.json'):
-        os.remove('totaldata.json')
-    url = 'https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports'
+    if os.path.exists("totaldata.json"):
+        os.remove("totaldata.json")
+    url = "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports"
     page = requests.get(url)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    table = soup.find('table', attrs={'class': 'files js-navigation-container js-active-navigation-container'})
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
+    soup = BeautifulSoup(page.content, "html.parser")
+    table = soup.find(
+        "table",
+        attrs={"class": "files js-navigation-container js-active-navigation-container"},
+    )
+
+    if table is None:
+        raise Exception("Something is wrong with BeautifulSoup in 'table'!")
+
+    table_body = table.find("tbody")
+    rows = table_body.find_all("tr")
     data = []
 
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
         data.append([ele for ele in cols if ele])
     csvnames = []
 
     for item in data:
-        if item[0] != 'README.md' and item[0] != 'Failed to load latest commit information.' and item[0]!='.gitignore':
+        if (
+            item[0] != "README.md"
+            and item[0] != "Failed to load latest commit information."
+            and item[0] != ".gitignore"
+        ):
             csvnames.append(item[0])
     count = 0
     for name in csvnames:
         dtdict = {}
         print(name)
-        lastelementurl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}'.format(name)
+        lastelementurl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{}".format(
+            name
+        )
         recent = requests.get(lastelementurl)
-        with open('global_daily/{}'.format(name), 'wb') as file:
+        with open("global_daily/{}".format(name), "wb") as file:
             file.write(recent.text.encode("utf-8"))
-        pd.set_option('display.max_rows', None)
-        df = pd.read_csv('global_daily/{}'.format(name), usecols=['Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered'])
+        pd.set_option("display.max_rows", None)
+        df = pd.read_csv(
+            "global_daily/{}".format(name),
+            usecols=[
+                "Country/Region",
+                "Last Update",
+                "Confirmed",
+                "Deaths",
+                "Recovered",
+            ],
+        )
         df.fillna("0", inplace=True)
-        df.to_csv('global_daily/{}'.format(name), index=False)
-        #print(df)
+        df.to_csv("global_daily/{}".format(name), index=False)
+        # print(df)
 
-    csvfiles = os.listdir('global_daily')
+    csvfiles = os.listdir("global_daily")
     print(csvfiles)
     for file in csvfiles:
-        df = pd.read_csv(f'global_daily/{file}',usecols=['Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered'])
-        print(f'Converting: {file} to json')
+        df = pd.read_csv(
+            f"global_daily/{file}",
+            usecols=[
+                "Country/Region",
+                "Last Update",
+                "Confirmed",
+                "Deaths",
+                "Recovered",
+            ],
+        )
+        print(f"Converting: {file} to json")
         csvtojsonfunction(df, file)
-    csvfiles = os.listdir('global_daily')
+    csvfiles = os.listdir("global_daily")
     for csv in csvfiles:
-        if csv.endswith('.csv'):
-            os.remove('global_daily/{}'.format(csv))
-
+        if csv.endswith(".csv"):
+            os.remove("global_daily/{}".format(csv))
 
     global_daily = Path("global_daily")
 
@@ -186,7 +256,7 @@ def get_data_from_all_to_json():
     for d in list_of_dicts:
         for key, value in d.items():
             master[key].append(value)
-    with open('totaldata.json', 'w') as file:
+    with open("totaldata.json", "w") as file:
         file.write(json.dumps(master))
     delglobal()
 
--- covidmap.py
+++ covidmap.py
@@ -5,15 +5,39 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from collecter import csvtojson
 import requests
+
 # CONFIG #
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///covidmap.db'
+class Config:
+    """Configuration class for easy storage of useful metadata including API keys"""
+
+    def __init__(self):
+        self.SECRET_KEY = self._get_env("SECRET_KEY")
+        self.NYTIMES_KEY = self._get_env("NYTIMES_KEY")
+
+    def _get_env(self, env: str, fallback: str = None) -> str:
+        """Gets env var (boilerplate interchangable)"""
+
+        if fallback:
+            try:
+                return os.environ[env]
+            except:
+                return fallback
+
+        return os.environ[env]
+
+
+app = Flask(__name__)
+config = Config()
+
+app.config["SECRET_KEY"] = config.SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///covidmap.db"
 
 db = SQLAlchemy(app)
 
 # MODELS #
+
 
 class Node(db.Model):
     """A single datapoint (usually a countries data)"""
@@ -35,43 +59,86 @@ class Node(db.Model):
         self.created = datetime.datetime.utcnow()
 
 
+class Newslet(db.Model):
+    """A single found article"""
+
+    id = db.Column(db.String(), primary_key=True)
+    title = db.Column(db.String())
+    lead_paragraph = db.Column(db.String())
+    created = db.Column(db.DateTime)
+
+    def __init__(self, id: str, title: str, lead_paragraph: str):
+        self.id = id
+        self.title = title
+        self.lead_paragraph = lead_paragraph
+        self.created = datetime.datetime.now()
+
+
 # ROUTES #
+
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", newslets=Newslet.query.all())
 
-#dummy route
 
-@app.route('/data', methods=['GET'])
+@app.route("/data", methods=["GET"])
 def passdata():
     try:
-        response = {'Success':'Data has been successfully obtained', 'Data': data_formatting()}
+        response = {
+            "Success": "Data has been successfully obtained",
+            "Data": data_formatting(),
+        }
         return response, 200
     except:
-        response = {'Error':'An error has occured.'}
+        response = {"Error": "An error has occured."}
         return response, 400
 
-@app.route('/totaldata', methods=['GET'])
+
+@app.route("/totaldata", methods=["GET"])
 def totaldata():
-    with open('totaldata.json', 'r') as file:
+    with open("totaldata.json", "r") as file:
         jsfile = json.load(file)
-        response = {'Success':'Data has been successfully obtained', 'Data': jsfile}
+        response = {"Success": "Data has been successfully obtained", "Data": jsfile}
     return response, 200
 
 
-@app.route('/coords', methods=['GET'])
+@app.route("/coords", methods=["GET"])
 def passkey():
-    with open('cords.json', 'r') as file:
-        response = {'Data': json.loads(file.read())}
+    with open("cords.json", "r") as file:
+        response = {"Data": json.loads(file.read())}
         return response, 200
 
-@app.route('/accesskey', methods=['GET'])
+
+@app.route("/accesskey", methods=["GET"])
 def accesskey():
-    #print(os.getenv('access_key'))
-    return {'key': os.environ['access_key']}
+    # print(os.getenv('access_key'))
+    return {"key": os.environ["access_key"]}
+
 
 # UTILS #
+
+
+def pull_nytimes() -> bool:
+    """Adds new nytimes stuff to database and returns if it was successful"""
+
+    search = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=coronavirus&api-key={config.NYTIMES_KEY}"
+    resp = requests.get(search)
+
+    if resp.status_code != 200:
+        return False
+
+    for newslet in resp.json()["response"]["docs"]:
+        if Newslet.query.filter_by(id=newslet["web_url"]).first() is None:
+            new_newslet = Newslet(
+                newslet["web_url"], newslet["snippet"], newslet["lead_paragraph"]
+            )
+
+            db.session.add(new_newslet)
+            db.session.commit()
+
+    return True
+
 
 def populate_db():
     """Top-level function for getting all csv data from github"""
@@ -81,12 +148,18 @@ def populate_db():
         print("Database is already populated!")
         return
 
+    print("Adding stats..")
     db.create_all()
     csv_data = json.loads(csvtojson())
     for country_name in csv_data:
         new_node = Node(country_name, csv_data[country_name])
         db.session.add(new_node)
         db.session.commit()
+
+    print("Adding newslets..")
+
+    if not pull_nytimes():
+        print("Failed to add newslets!")
 
 
 def data_formatting():
@@ -98,18 +171,19 @@ def data_formatting():
             countries.append(row.country_name)
     for country in countries:
         for row in data:
-            if row.country_name==country:
-                datadict[country]= {
-                    'confirmed': row.confirmed,
-                    'deaths': row.deceased,
-                    'recovered': row.recovered
+            if row.country_name == country:
+                datadict[country] = {
+                    "confirmed": row.confirmed,
+                    "deaths": row.deceased,
+                    "recovered": row.recovered,
                 }
-               # print(datadict[country]['recovered'])
+            # print(datadict[country]['recovered'])
     return datadict
+
 
 def get_coords():
     if os.path.exists("cords.json"):
-        print('Coords have already been gathered')
+        print("Coords have already been gathered")
     else:
         data = Node.query.all()
         countries = []
@@ -118,20 +192,27 @@ def get_coords():
             if row.country_name not in countries:
                 countries.append(row.country_name)
         for country in countries:
-            if country == 'Mainland China':
-                countryname = 'China'
-            elif country == 'occupied Palestinian territory':
-                countryname = 'Palestine'
+            if country == "Mainland China":
+                countryname = "China"
+            elif country == "occupied Palestinian territory":
+                countryname = "Palestine"
             else:
                 countryname = country
-            response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyB-2lC7PWWHcDvc6T6mtVdmXCzfGf_p0kA'.format(countryname))
+            response = requests.get(
+                "https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyB-2lC7PWWHcDvc6T6mtVdmXCzfGf_p0kA".format(
+                    countryname
+                )
+            )
             dt = json.loads(response.text)
-            #print(dt)
-            #print(country)
-            cordsdict[country] = {'Latitude': dt['results'][0]['geometry']['location']['lat'], 'Longitude': dt['results'][0]['geometry']['location']['lng']}
-            #print('Country: {}'.format(country) + str(dt['results'][0]['geometry']['location']))
-            #print(cordsdict)
-            with open('cords.json', 'w') as file:
+            # print(dt)
+            # print(country)
+            cordsdict[country] = {
+                "Latitude": dt["results"][0]["geometry"]["location"]["lat"],
+                "Longitude": dt["results"][0]["geometry"]["location"]["lng"],
+            }
+            # print('Country: {}'.format(country) + str(dt['results'][0]['geometry']['location']))
+            # print(cordsdict)
+            with open("cords.json", "w") as file:
                 file.write(json.dumps(cordsdict))
 
     data = Node.query.all()
@@ -141,19 +222,24 @@ def get_coords():
         if row.country_name not in countries:
             countries.append(row.country_name)
     for country in countries:
-        if country == 'Mainland China':
-            countryname = 'China'
-        elif country == 'occupied Palestinian territory':
-            countryname = 'Palestine'
-        response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyB-2lC7PWWHcDvc6T6mtVdmXCzfGf_p0kA'.format(countryname))
+        if country == "Mainland China":
+            countryname = "China"
+        elif country == "occupied Palestinian territory":
+            countryname = "Palestine"
+        response = requests.get(
+            "https://maps.googleapis.com/maps/api/geocode/json?address={}&key=AIzaSyB-2lC7PWWHcDvc6T6mtVdmXCzfGf_p0kA".format(
+                countryname
+            )
+        )
         dt = json.loads(response.text)
-        #print(dt)
-        #print(country)
-        cordsdict[country] = {'Latitude': dt['results'][0]['geometry']['location']['lat'], 'Longitude': dt['results'][0]['geometry']['location']['lng']}
-        #print('Country: {}'.format(country) + str(dt['results'][0]['geometry']['location']))
-    #print(cordsdict)
-
-
+        # print(dt)
+        # print(country)
+        cordsdict[country] = {
+            "Latitude": dt["results"][0]["geometry"]["location"]["lat"],
+            "Longitude": dt["results"][0]["geometry"]["location"]["lng"],
+        }
+        # print('Country: {}'.format(country) + str(dt['results'][0]['geometry']['location']))
+    # print(cordsdict)
 
 
 if __name__ == "__main__":
